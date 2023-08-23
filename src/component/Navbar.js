6a75c09c83950007c6a8d4ef7be3120806c1089a
Anshul Rai <anshulrai077@gmail.com>
2023-08-23 09:27:25 +0530

    fixed the title

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -6,7 +6,7 @@ export default function Navbar(props) {
     return (
         <nav className={`navbar bg-${props.mode} navbar-expand-lg border-bottom border-body`} data-bs-theme={`${props.mode}`}>
             <div className="container-fluid">
-                <a className="navbar-brand" href="/">{props.title}</a>
+                <a className="navbar-brand" href="#">{props.title}</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
